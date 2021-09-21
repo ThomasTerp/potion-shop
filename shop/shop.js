6d735b828b86238803bc6c546707d5308077d253
Thomas Terp <thomascterp@gmail.com>
2021-09-21 14:57:17 +0200

    Made potions interactive

--- shop/shop.js
+++ shop/shop.js
@@ -1,60 +1,89 @@
 import "../main/jquery-3.6.0.min.js"
 import "../main/circletype.min.js";
 import config from "../main/config.js"
-import Potion from "./potion.js"
 
 // Wait for page to load
 $(() =>
 {
+	const cartPotions = [];
+
 	// Resize elements when the page has changed size
 	const resize = () =>
 	{
+		// Shelf width snapped to potion width
 		let width = Math.floor($(window).width() / config.potionWidth) * config.potionWidth;
 
+		// Resize shelf
 		$(".shelf").css({
 			backgroundSize: `${config.shelfImageWidth}px auto`,
 			width: `${width}px`
 		});
 
+		// Resize potions
 		$(".potion").css({
 			width: `${config.potionWidth}px`,
 			height: `${config.potionHeight}px`
 		});
 	};
 	resize();
-	$(window).resize(() => resize());
+	$(window).on("resize", () => resize());
+
+	// Build potion HTML from a potion object
+	const buildPotion = (potion, isInteractive, $container) =>
+	{
+		// Build the HTML
+		const $potion = $(`
+			<div class="potion ${isInteractive ? "interactive" : ""}" style="width: ${config.potionWidth}px; height: ${config.potionHeight}px;">
+				<img class="photo" src="${potion.imageSrc}" alt="${potion.title}" />
+				<div class="title">
+				${potion.title}
+				</div>
+				<div class="tag">
+				$${potion.price}
+				</div>
+				<div class="addToCart">
+					Add to cart
+				</div>
+			</div>
+		`);
+
+		if(isInteractive)
+		{
+			$potion.on("click", () =>
+			{
+				const $cartPotion = buildPotion(potion, false, $(".cart"));
+				$cartPotion.css({
+					position: "absolute",
+					left: `${$potion.offset().left}px`,
+					top: `${$potion.offset().top}px`
+				});
+				$cartPotion.animate({
+					left: `${cartPotions.length * 80}px`,
+					top: "200px"
+				})
+				cartPotions.push($cartPotion);
+			});
+		}
+
+		// Append the potion HTML to the container element
+		$container.append($potion);
+
+		// Curve the potion title
+		new CircleType($potion.find(".title")[0])
+			.radius(300);
+
+		return $potion;
+	};
 
 	// Get potions array from API
-	$.getJSON("../api/potions.json", (potionsData) =>
+	$.getJSON("../api/potions.json", (potions) =>
 	{
-		// Loop through the data for potions
-		for(const potionData of potionsData)
-		{
-			// Create a Potion object from the data
-			const potion = new Potion(potionData.id, potionData.title, potionData.price, potionData.imageSrc);
-			// Create HTML for the potion
-			const $potion = $(`
-				<div class="potion">
-					<img class="photo" src="${potion.imageSrc}" alt="${potion.title}" />
-					<div class="title">
-						${potion.title}
-					</div>
-					<div class="tag">
-						$${potion.price}
-					</div>
-				</div>
-			`);
-			$potion.data(potion);
-			$potion.css({
-				width: `${config.potionWidth}px`,
-				height: `${config.potionHeight}px`
-			});
-			// Append the potion HTML to the .shelf element
-			$(".shelf").append($potion);
+		const $shelf = $(".shelf");
 
-			// Curve the potoin title
-			new CircleType($potion.find(".title")[0])
-				.radius(300);
+		// Loop through the data for potions
+		for(const potion of potions)
+		{
+			buildPotion(potion, true, $shelf);
 		}
 	});
 });
